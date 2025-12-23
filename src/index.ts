9447e7bdfd59cece2e836e770aebb2522665ea55
Guilherme Ferreira <guilhermefreichert@gmail.com>
2025-12-23 12:21:31 -0300

    continue

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+async function main() {
+  const students = await db.student.findMany();
+  console.log(students);
+}
+
+main();
+    
